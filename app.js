ba5c06499ab98d25750e947b4184a4016aa37a49
Cerjjapan <cerjjapan@yahoo.com>
2018-08-27 05:00:03 +0000

    changed app.js

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes       = require("./routes/comments"),
     indexRoutes         = require("./routes/index")
   
 
-mongoose.connect("mongodb://localhost/yelp_camp_12");
+//mongoose.connect("mongodb://localhost/yelp_camp_12")
+mongoose.connect("mongodb://cerj:Eiko2018@ds133632.mlab.com:33632/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
